4d4a4ece982f50abe2d6cb7f20d8afb8927411e2
Vaishnavi Verma <vaiatsunrise@gmail.com>
2025-08-13 19:57:23 +0530

    Add files via upload

---
+++ Display Array Elements.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {27, 13, 15, 22, 15};
+
+    for (int i = 0; i < 5; i++) {
+        cout << marks[i] << endl;
+    }
+    return 0;
+}
